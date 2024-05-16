a75b8777d8f18b3b72d10afd79047d03a5c350d3
juandiego1150 <juandiegoreyesrabel@gmail.com>
2024-05-16 16:18:14 +0200

    xammp corrupto copia de seguridad ultimos cambios

--- proyectos/spar/src/php/views/register.php
+++ proyectos/spar/src/php/views/register.php
@@ -27,3 +27,78 @@
     </div>
     <button id="cerrar">Aceptar</button>
 </div>
+<script>
+     $(document).ready(function() {
+        $("#registrarse").click(function() {
+            $.ajax({
+                type: "POST",
+                url: "controllers/usuarios.php",
+                dataType: "Text",
+                data: {
+                    option: 2,
+                    nombre: $("#nombreRegistro").val(),
+                    contraseña: $("#contraseñaRegistro").val()
+                },
+                success: function(a) {
+                    $("#popupBody").html(a);
+                    openPopup();
+                }
+            });
+        });
+
+        $("#login").click(function() {
+            $.ajax({
+                type: "POST",
+                url: "controllers/usuarios.php",
+                dataType: "Text",
+                data: {
+                    option: 4,
+                    nombre: $("#nombreLogin").val(),
+                    contraseña: $("#contraseñaLogin").val()
+                },
+                success: function(a) {
+                    console.log(a);
+                        switch (a) {
+                            case "1":
+                                a = "faltan datos";
+                                $("#alerta").html(a);
+                                break;
+                            case "3":
+                                a = "usuario o contraseña incorrectos";
+                                $("#alerta").html(a);
+                                break;
+                            case "2":
+                                a = "Bienvenido";
+                                openPopup();
+                                $("#popupBody").html(a);
+                                break;
+                        }
+                }  
+            });
+        });
+          //POPUP SCRIPT
+          $("#cerrar").click(function() {
+                closePopup();
+            });
+        function openPopup() {
+            document.getElementById('overlay').style.display = 'block';
+            document.getElementById('popup').style.display = 'block';
+        }
+
+        // Función para cerrar el pop-up
+        function closePopup() {
+            document.getElementById('overlay').style.display = 'none';
+            document.getElementById('popup').style.display = 'none';
+        }
+        //FIN POPUP SCRIPT
+        //registro script
+
+        $('.message a').click(function() {
+            $('form').animate({
+                height: "toggle",
+                opacity: "toggle"
+            }, "slow");
+        });
+        //fin registro script
+    });
+</script>
